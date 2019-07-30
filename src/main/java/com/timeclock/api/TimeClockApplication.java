11b1ebaa3ba1ee67d1f87309ceaa5047be6401da
Pedro Renzo Vicentin <rapedro_8@hotmail.com>
2019-07-30 01:07:36 -0300

    Foundation

---
+++ src/main/java/com/timeclock/api/TimeClockApplication.java
@@ -0,0 +1,13 @@
+package com.timeclock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeClockApplication.class, args);
+	}
+
+}
