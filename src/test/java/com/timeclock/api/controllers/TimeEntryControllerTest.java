ae38441e89fe7cb5cd7d1d37ec0cdc9001172145
Pedro Renzo Vicentin <rapedro_8@hotmail.com>
2020-01-27 21:11:20 +0000

    Adjusting Time Entry Controller test.

--- src/test/java/com/timeclock/api/controllers/TimeEntryControllerTest.java
+++ src/test/java/com/timeclock/api/controllers/TimeEntryControllerTest.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
@@ -25,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -99,9 +99,8 @@ public class TimeEntryControllerTest {
 				.andExpect(status().isOk());
 	}
 
-	@Test
+	@Test(expected = NestedServletException.class)
 	@WithMockUser
-	@Ignore
 	public void testRemoveTimeEntryAccessForbidden() throws Exception {
 		BDDMockito.given(this.timeEntryService.findById(Mockito.anyLong())).willReturn(Optional.of(new TimeEntry()));
 
